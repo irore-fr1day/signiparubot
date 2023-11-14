9202f8448aec3dc78a492912370d1e03e2aeeefa
irore-fr1day <131448923+irore-fr1day@users.noreply.github.com>
2023-11-15 04:56:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -121,9 +121,25 @@ def get_value_from_url(message):
         return
     
     url = message.text
+    headers_list = [{ 
+	'authority': 'httpbin.org', 
+	'cache-control': 'max-age=0', 
+	'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"', 
+	'sec-ch-ua-mobile': '?0', 
+	'upgrade-insecure-requests': '1', 
+	'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+	'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 
+	'sec-fetch-site': 'none', 
+	'sec-fetch-mode': 'navigate', 
+	'sec-fetch-user': '?1', 
+	'sec-fetch-dest': 'document', 
+	'accept-language': 'en-US,en;q=0.9', 
+    } # , {...} 
+    ] 
+    headers = random.choice(headers_list) 
     
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
